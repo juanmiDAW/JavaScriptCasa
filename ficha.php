d817f12714dde6813b05a3c55d71fed88aab9bfe
juanmiDAW <juanmiguel.valle@iesdonana.org>
2024-09-24 18:27:02 +0200

    carga inicial

---
+++ ficha.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Esto es el resultado del formulario</p>    
+    <?php
+        $x=6;
+        $y=2;
+        $z=$x+$y;
+        echo ($z);
+    ?>
+    <br>
+    <?="Hola esto es php aunque no lo parezca"?>
+</body>
+</html>
